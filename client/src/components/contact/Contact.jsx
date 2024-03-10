58dc5b5171256f36beb5c4acf06b9e29e72d51fb
Devendra-Kumar-Dhayal <101494090+Devendra-Kumar-Dhayal@users.noreply.github.com>
2024-03-11 03:54:37 +0530

    Update Contact.jsx

--- client/src/components/contact/Contact.jsx
+++ client/src/components/contact/Contact.jsx
@@ -58,15 +58,15 @@ const Contact = () => {
         <motion.h1 variants={variants}>Let’s work together</motion.h1>
         <motion.div className="item" variants={variants}>
           <h2>Mail</h2>
-          <span>hello@react.dev</span>
+          <span>devendra1203dhayal@gmail.com</span>
         </motion.div>
         <motion.div className="item" variants={variants}>
           <h2>Address</h2>
-          <span>Hello street New York</span>
+          <span>Manipal University Jaipur</span>
         </motion.div>
         <motion.div className="item" variants={variants}>
           <h2>Phone</h2>
-          <span>+1 234 5678</span>
+          <span>+91 63779 02280</span>
         </motion.div>
       </motion.div>
       <div className="formContainer">
